462a163c2e74df988afca3ed9887b098b65807b2
sandip <sandipbind720@gmail.com>
2024-12-12 22:22:48 +0530

    card responcive

--- components/cart/VerifyLocation.js
+++ components/cart/VerifyLocation.js
@@ -4,8 +4,8 @@ import LocationButton from "components/style/LocationButton";
 import Box from "@mui/material/Box";
 const VerifyLocation = React.memo(function VerifyNumberC(props) {
   return (
-    <CartAccordion id="verify-location" title="Verify your location">
-      <Box sx={{ mx:15, }}><LocationButton hideButton={true} /></Box>
+    <CartAccordion id="verify-location" title="Verify your location" >
+      <Box  sx={{mx:15, marginLeft:{xs:"0px",sm:"0px"},marginRight:{xs:"0px",sm:"0px"}}}><LocationButton hideButton={true}  /></Box>
       
     </CartAccordion>
   );
