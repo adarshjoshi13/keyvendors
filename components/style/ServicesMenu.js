fc9f15c52a75fed1fece66baaa4321700a1a5d0f
sandip <sandipbind720@gmail.com>
2024-12-15 20:39:05 +0530

    header

--- components/style/ServicesMenu.js
+++ components/style/ServicesMenu.js
@@ -4,45 +4,73 @@ import Box from "@mui/material/Box";
 import React, { memo } from "react";
 import { useLocation } from "utils/hooks";
 import { getServiceUrl } from "utils/utility";
+import { validateConfig } from "next/dist/server/config-shared";
 
-export default memo(function ServicesMenu({ items ,sx }) {
-  const location =useLocation();
+export default memo(function ServicesMenu({ items, sx }) {
+  const location = useLocation();
   return (
     <Grid
-      sx={{ m: 2, maxHeight: "20rem", width: "100%",...sx }}
+      sx={{ m: 2, maxHeight: "20rem", width: "100%", ...sx }}
       justifyContent="start"
-      direction={"row"}
+      direction="row"
       alignItems="start"
-      alignContent={"start"}
+      alignContent="start"
       flexWrap
-      container>
+      container
+    >
       {items &&
         items.map(
           (item, index) =>
             item.children.length > 0 && (
-              <Grid lg={4} md={4} xs={6} key={index} order={item.children.length} item>
+              <Grid
+                lg={4}
+                md={4}
+                xs={6}
+                key={index}
+                order={item.children.length}
+                item
+              >
                 <Box
                   sx={{
                     mb: 0.5,
                     fontSize: 14,
                     textTransform: "capitalize",
-                  }}>
+                    display: "flex",
+                    alignItems: "center",
+                    paddingTop:"10px",
+                    gap: 1, // Space between the icon and text
+                  }}
+                >
+                  {item.icon && (
+                    <img
+                      src={item.icon}
+                      alt={`${item.title} icon`}
+                      style={{
+                        width: "20px",
+                        height: "20px",
+                        objectFit: "contain",
+                      }}
+                    />
+                  )}
                   <Link
                     {...(item.isClick
                       ? { onClick: item.method }
                       : { href: `/service/${location.slug}/${item.slug}` })}
-                    underline="none">
+                    underline="none"
+                  >
                     {item.title}
                   </Link>
                 </Box>
                 {item.children.map((child, ind) => (
                   <Box
                     key={ind}
-                    sx={{ fontSize: 12, textTransform: "capitalize" }}>
+                    sx={{ fontSize: 12, textTransform: "capitalize" }}
+                  >
                     <Link
-                      href={getServiceUrl(location,`${item.slug}/${child.slug}`)}
+                      href={getServiceUrl(location, `${item.slug}/${child.slug}`)}
                       underline="none"
-                      color="#000000">
+                      color="#000000"
+                    >
                       {child.title}
                     </Link>
                   </Box>
