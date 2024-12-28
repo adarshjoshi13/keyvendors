ab10c26b8f88330ea4cb71e1effa382e294072fb
sandip kumar <sandipbind720@gmail.com>
2024-12-28 14:05:02 +0530

    update

--- next.config.js
+++ next.config.js
@@ -24,10 +24,10 @@ const rewrites = async (API_URL) => {
 
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    //largePageDataBytes: 128 * 1000, // 128KB by default
-      largePageDataBytes: 128 * 100000,
-    },
+  // experimental: {
+  //   //largePageDataBytes: 128 * 1000, // 128KB by default
+  //     largePageDataBytes: 128 * 100000,
+  //   },
   env: {
     HOST: process.env.HOST,
     PAY_URL: process.env.PAY_URL,
